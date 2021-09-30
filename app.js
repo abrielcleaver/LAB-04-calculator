7422d9ca4101aee10f9328aa4f88fc85e7eb66ee
Abriel Cleaver <abrielcleaver@gmail.com>
2021-09-30 16:34:31 -0700

    add division elements in HTML and JS

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-import {add} from './calculations.js';
+import {add, subtraction, multiplication, division} from './calculations.js';
 
+// Addition
 const addInput1 = document.getElementById('add-input-1');
 const addInput2 = document.getElementById('add-input-2');
 const addButton = document.getElementById('add-button');
@@ -13,8 +14,7 @@ addButton.addEventListener('click', () =>{
   addAnswer.textContent = result;
 });
 
-import {subtraction} from './calculations.js';
-
+// Subtraction
 const subInput1 = document.getElementById('sub-input-1');
 const subInput2 = document.getElementById('sub-input-2');
 const subButton = document.getElementById('sub-button');
@@ -28,9 +28,7 @@ subButton.addEventListener('click', () =>{
   subAnswer.textContent = result;
 })
 
-
-import { multiplication } from './calculations.js';
-
+// Multiplication
 const multiplyInput1 = document.getElementById('multiply-input-1');
 const multiplyInput2 = document.getElementById('multiply-input-2');
 const multiplyButton = document.getElementById('multiply-button');
@@ -45,3 +43,16 @@ multiplyButton.addEventListener('click', () => {
 
 });
 
+// Division
+const divideInput1 = document.getElementById('divide-input-1');
+const divideInput2 = document.getElementById('divide-input-2');
+const divideButton = document.getElementById('divide-button');
+const divideAnswer = document.getElementById('divide-answer');
+
+divideButton.addEventListener('click', () => {
+  const value7 = Number(divideInput1.value);
+  const value8 = Number(divideInput2.value);
+  const result = division(value7, value8);
+  console.log(result);
+  divideAnswer.textContent = result;
+});
