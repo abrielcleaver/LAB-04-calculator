5374f1db532a71a2d14b20ad89bda093a36df469
Abriel Cleaver <abrielcleaver@gmail.com>
2021-09-30 16:03:14 -0700

    add subtraction elements in HTML and JS

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import {add} from './calculations.js';
 const addInput1 = document.getElementById('add-input-1');
 const addInput2 = document.getElementById('add-input-2');
 const addButton = document.getElementById('add-button');
-const addAnswer = document.getElementById('add-answer')
+const addAnswer = document.getElementById('add-answer');
 
 addButton.addEventListener('click', () =>{
   const value1 = Number(addInput1.value);
@@ -13,5 +13,18 @@ addButton.addEventListener('click', () =>{
   addAnswer.textContent = result;
 });
 
+import {subtraction} from './calculations.js';
 
+const subInput1 = document.getElementById('sub-input-1');
+const subInput2 = document.getElementById('sub-input-2');
+const subButton = document.getElementById('sub-button');
+const subAnswer = document.getElementById('sub-answer');
+
+subButton.addEventListener('click', () =>{
+  const value3 = Number(subInput1.value);
+  const value4 = Number(subInput2.value);
+  const result = subtraction(value3, value4);
+  console.log(result);
+  subAnswer.textContent = result;
+})
 
